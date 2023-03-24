f9e7ce26cfcb5edb68cecfd36222058022284cd0
Marcel Schliwka <marcelschliwka@protonmail.com>
2023-03-24 16:52:31 +0100

    html boilerplate, assets and game board added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Tic Tac Toe App - Copyright Marcel Schliwka - 2023
+
+// Global Variables
+// Fields
+let field1 = document.getElementById("field1");
+let field2 = document.getElementById("field2");
+let field3 = document.getElementById("field3");
+let field4 = document.getElementById("field4");
+let field5 = document.getElementById("field5");
+let field6 = document.getElementById("field6");
+let field7 = document.getElementById("field7");
+let field8 = document.getElementById("field8");
+let field9 = document.getElementById("field9");
